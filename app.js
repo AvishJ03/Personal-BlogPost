58f27b776ce68b2100420f7fc168e8f2a39e98e7
Avish Jain <avishjain0@gmail.com>
2022-03-18 01:20:48 +0530

    mongo

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require("lodash");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/postsDB");
+
+const postSchema = {
+  title: String,
+  body: String,
+};
+
+const Post = mongoose.model("Post", postSchema);
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -11,8 +21,6 @@ const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
-let posts = [];
 
 const app = express();
 
@@ -22,7 +30,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", { startContent: homeStartingContent, posts: posts });
+  Post.find({}, function (err, post) {
+    if (err) {
+      console.log(err);
+    } else {
+      const posts = [];
+      post.forEach((element) => {
+        const p = {
+          title: lodash.capitalize(element.title),
+          body: element.body,
+        };
+        posts.push(p);
+      });
+      res.render("home", { startContent: homeStartingContent, posts: posts });
+    }
+  });
 });
 
 app.get("/about", function (req, res) {
@@ -38,21 +60,25 @@ app.get("/compose", function (req, res) {
 });
 
 app.get("/posts/:title", function (req, res) {
-  const title = lodash.lowerCase(req.params.title);
-  posts.forEach((element) => {
-    const ele = lodash.lowerCase(element.title);
-    if (ele == title) {
-      res.render("post", { title: element.title, content: element.body });
+  const titl = lodash.lowerCase(req.params.title);
+  Post.findOne({ title: titl }, function (err, element) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("post", {
+        title: lodash.capitalize(element.title),
+        content: element.body,
+      });
     }
   });
 });
 
 app.post("/compose", function (req, res) {
-  const post = {
-    title: req.body.postTitle,
+  const post = new Post({
+    title: lodash.lowerCase(req.body.postTitle),
     body: req.body.postBody,
-  };
-  posts.push(post);
+  });
+  post.save();
   res.redirect("/");
 });
 
